28ce0614a724f3b5f4fe310b21c3f0a5ca2cbf50
Marc-Antoine Willy <marcantoinewilly@pm.me>
2025-11-24 15:42:41 +0100

    Switch to Arch Linux

--- CachyOS/tofi/launcher.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-selection=$(compgen -c | sort -u | tofi --prompt-text "Run" || true)
-[ -z "${selection}" ] && exit 0
-
-niri msg spawn -- sh -lc "${selection}"
